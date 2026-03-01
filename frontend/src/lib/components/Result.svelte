b37242d3e47e2261315f37a19158ce5e3e7d6fe9
Zane Shaw <zane@squidee.dev>
2026-03-02 01:23:51 +1100

    frontend: improve embed rendering

--- frontend/src/lib/components/Result.svelte
+++ frontend/src/lib/components/Result.svelte
@@ -16,10 +16,10 @@
 </script>
 
 <div class="outline-liam-background flex w-fit flex-col gap-2 rounded p-2.5 outline-1 transition-[outline] duration-150 hover:outline-gray-700">
-	<div class="relative">
+	<div class="relative flex items-center justify-center overflow-hidden rounded-sm">
 		{#if showEmbed}
 			<iframe
-				src="https://www.youtube.com/embed/{result.video.id}?start={result.seconds}&autoplay=1"
+				src="https://www.youtube.com/embed/{result.video.id}?start={result.seconds}&autoplay=1&rel=0"
 				title={result.video.title}
 				frameborder="0"
 				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -28,8 +28,8 @@
 				class="absolute top-0 left-0 z-10 size-full"
 			></iframe>
 		{/if}
-		<button onclick={() => (showEmbed = true)} class="relative cursor-pointer">
-			<img src={result.video.thumbnailUrl} alt="" class="aspect-video w-full rounded-sm" />
+		<button class:invisible={showEmbed} onclick={() => (showEmbed = true)} class="relative cursor-pointer">
+			<img src={result.video.thumbnailUrl} alt="" class="aspect-video w-full" />
 			<svg viewBox="0 0 68 48" class="absolute top-1/2 left-1/2 size-1/4 -translate-1/2 fill-[#ff0033]">
 				<path
 					d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"
