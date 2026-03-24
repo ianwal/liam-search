a2d72674387fdd9c6fef9f805ff7b496e3e1b78e
Zane Shaw <zane@squidee.dev>
2026-03-24 22:09:58 +1100

    backend: update videos cache on interval

--- backend/src/jobs/registerInterval.ts
+++ backend/src/jobs/registerInterval.ts
@@ -1,13 +1,15 @@
+import { Job } from "@zaneshaw/squeue";
+
 import config from "../config";
 import { queue } from "../main";
-import { Job } from "@zaneshaw/squeue";
 import processVideos from "./processVideos";
+import updateVideosCache from "./updateVideosCache";
 
 export default new Job("register interval", () => {
 	setInterval(
 		() => {
-			// todo: check if job is already running
-			queue.push(processVideos);
+			// todo: check if jobs are already running
+			queue.push(updateVideosCache, processVideos);
 		},
 		config.core.process_interval * 60 * 1000,
 	);
